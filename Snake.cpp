0713e0c15b67bd08860c90dbf5c4082f6675d276
amir <amir.hamze.sahraki@gmail.com>
2023-03-20 18:37:29 +0330

    update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -3,9 +3,8 @@
 #include <windows.h> // Sleep()
 #include <thread>    // class thread
 
-#include "random.h"  //rendom()
-#include "gotoxy.h"  // gotoxy()
-
+#include "random.h"
+#include "gotoxy.h"
 using namespace std;
 
 #define up_key 72
@@ -14,15 +13,24 @@ using namespace std;
 #define left_key 75
 #define Esc_key 27
 
-int score = 0,
-    x = 10,
-    y = 11;
+int score = 0;
+char input = 0;
+struct Snake{
+    int x = 10,
+        y = 11;
+};Snake snake;
 
-char face = 2,
-    input = 0;
+struct Food{
+    int x,y;
+    char face = 2;
+};Food food;
 
-int randx = random(2, 77);
-int randy = random(1, 23);
+struct Wallborder{
+    int top = 1,
+        buttom = 23,
+        left = 2,
+        right = 77;
+};Wallborder wall;
 
 void move_up();
 void move_down();
@@ -41,19 +49,19 @@ int main()
         switch (input)
         {
             case up_key:
-                if(y>1)
+                if(snake.y >wall.top)
                     move_up();
                 break;
             case down_key:
-                if(y<23)
+                if(snake.y<wall.buttom)
                     move_down();
                 break;
             case right_key:
-                if(x<77)
+                if(snake.x<wall.right)
                     move_right();
                 break;
             case left_key:
-                if(x>2)
+                if(snake.x>wall.left)
                     move_left();
                 break;
             case Esc_key:
@@ -90,60 +98,64 @@ void intro()
     for (int i=2,j=77; i<78 && j>1 ;i++,j--)
         {gotoxy(i, 0);cout << "="; gotoxy(j, 24);cout << "=";Sleep(10);}
 
+    food.x = random(wall.left, wall.right);
+    food.y = random(wall.top, wall.buttom);
     gotoxy(2, 0);   cout << " score:" << score << "  ";
-    gotoxy(3, 25);  cout << "by:Amir Hamzeh";
+    gotoxy(3, 25);  cout << "Creator:Amir Hamzeh";
     gotoxy(32, 25); cout << "use keys" <<'['<< (char) 24 <<','<<(char) 25 << ',' << (char) 26 << ',' << '<' <<']';
     gotoxy(65, 25); cout << "EXIT = [Esc]";
-    gotoxy(randx, randy);cout.put(face);
-    gotoxy(x, y); cout.put(26); gotoxy(x, y);
+    gotoxy(food.x, food.y);cout.put(food.face);
+    gotoxy(snake.x, snake.y); cout.put(26); gotoxy(snake.x, snake.y);
 
 }
 void check_place()
 {
-    if (x == randx && y == randy)
+    if (snake.x == food.x && snake.y == food.y)
     {
         gotoxy(2, 0); cout << " "<< "score:" << ++score << "  ";
 
-        randx = random(2, 77);
-        randy = random(1, 23);
+        food.x = random(wall.left, wall.right);
+        food.y = random(wall.top, wall.buttom);
 
-        gotoxy(randx, randy); cout.put(face);
-        gotoxy(x, y); cout.put(' ');
-        x = 10;
-        y = 11;
-        gotoxy(x, y); cout.put(26); gotoxy(x, y);
+        gotoxy(food.x, food.y); cout.put(food.face);
+        gotoxy(snake.x, snake.y); cout.put(' ');
+        gotoxy(snake.x, snake.y); cout.put(26); gotoxy(snake.x, snake.y);
     }
 }
 void move_up()
 {
     cout.put(' ');
-    gotoxy(x, --y);
+    snake.y--;
+    gotoxy(snake.x,snake.y);
     cout.put(24);//↑
-    gotoxy(x, y);
+    gotoxy(snake.x, snake.y);
     check_place();
 }
 void move_down()
 {
     cout.put(' ');
-    gotoxy(x, ++y);
+    snake.y++;
+    gotoxy(snake.x, snake.y);
     cout.put(25);//↓
-    gotoxy(x, y);
+    gotoxy(snake.x, snake.y);
     check_place();
 }
 void move_right()
 {
     cout.put(' ');
-    gotoxy(++x, y);
+    snake.x++;
+    gotoxy(snake.x, snake.y);
     cout.put(26);//→
-    gotoxy(x, y);
+    gotoxy(snake.x, snake.y);
     check_place();
 }
 void move_left()
 {
     cout.put(' ');
-    gotoxy(--x, y);
+    snake.x--;
+    gotoxy(snake.x, snake.y);
     cout<<'<';//←
-    gotoxy(x, y);
+    gotoxy(snake.x, snake.y);
     check_place();
 }
 void key_press()
