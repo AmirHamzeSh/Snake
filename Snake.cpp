42a1ec909ee5797cfbb926fbec91a8ed0332d62d
amir <amir.hamze.sahraki@gmail.com>
2023-03-21 16:58:39 +0330

    add tail for snake

--- Snake.cpp
+++ Snake.cpp
@@ -14,9 +14,12 @@ using namespace std;
 
 int score = 0;
 char input = 0;
+char pre_input = 0 ;
 struct Snake{
     int x = 10,
-        y = 11;
+        y = 11,
+        tail_XY[100][100];
+        int lenTail = 0;
 };
 struct Food{
     int x,y;
@@ -33,6 +36,7 @@ Snake snake;
 Food food;
 Wallborder wall;
 
+void check_place();
 void move_up();
 void move_down();
 void move_right();
@@ -47,42 +51,63 @@ int main()
     {
         if(_kbhit())
             input = getch();
-        
+
         switch (input)
         {
             case up_key:
-                if(snake.y >wall.top)
-                    move_up();
-                Sleep(100);
+                move_up();
+                Sleep(90);
                 break;
             case down_key:
-                if(snake.y<wall.buttom)
-                    move_down();
-                Sleep(100);
+                move_down();
+                Sleep(90);
                 break;
             case right_key:
-                if(snake.x<wall.right)
-                    move_right();
+                move_right();
                 Sleep(50);
                 break;
             case left_key:
-                if(snake.x>wall.left)
-                    move_left();
+                move_left();
                 Sleep(50);
                 break;
             case Esc_key:
-                goto exit;
+                system("cls");
+                return 0;
+                break;
+            default:
+                input = getch();
                 break;
         }
-        
+        if(snake.x > wall.right 
+        || snake.x < wall.left 
+        || snake.y < wall.top 
+        || snake.y > wall.buttom)
+           break;
     }
 
-    exit:
+    Sleep(500);
+    gotoxy(35,12);
+    cout<<"you lose!";
+    gotoxy(25,13);
+    cout<<"press Enter to Restart or Exit";
+    if(getche() == 13)
+        main();
+
     system("cls");
     return 0;
 }
 void intro()
 {
+    score = 0;
+    input = 0;
+    snake.x = 10;
+    snake.y = 11;
+    snake.tail_XY[0][0] = snake.x;
+    snake.tail_XY[0][1] = snake.y;
+    snake.lenTail = 0;
+    food.x = random(wall.left, wall.right);
+    food.y = random(wall.top, wall.buttom);
+
     system("cls");
     system("title snake");
     system("mode con cols=80 lines=26");
@@ -93,8 +118,6 @@ void intro()
     for (int i=2,j=77; i<78 && j>1 ;i++,j--)
         {gotoxy(i, 0);cout << "="; gotoxy(j, 24);cout << "=";Sleep(10);}
 
-    food.x = random(wall.left, wall.right);
-    food.y = random(wall.top, wall.buttom);
     gotoxy(2, 0);   cout << " score:" << score << "  ";
     gotoxy(3, 25);  cout << "Creator:Amir Hamzeh";
     gotoxy(32, 25); cout << "use keys" <<'['<< (char) 24 <<','<<(char) 25 << ',' << (char) 26 << ',' << '<' <<']';
@@ -107,8 +130,9 @@ void check_place()
 {
     if (snake.x == food.x && snake.y == food.y)
     {
-        cout.put(' ');
-        gotoxy(2, 0); cout << " "<< "score:" << ++score << "  ";
+        snake.lenTail++;
+        score+=10;
+        gotoxy(2, 0); cout << ' ' << "score:" << score << ' ';
 
         food.x = random(wall.left, wall.right);
         food.y = random(wall.top, wall.buttom);
@@ -116,6 +140,22 @@ void check_place()
         gotoxy(food.x, food.y); cout.put(food.face);
         gotoxy(snake.x, snake.y);
     }
+
+    if(snake.lenTail>0)
+    {
+        gotoxy(snake.tail_XY[snake.lenTail][0],snake.tail_XY[snake.lenTail][1]);
+        cout.put(' ');
+
+        for(int i=snake.lenTail;i>0;i--)
+        {
+            snake.tail_XY[i][0] = snake.tail_XY[i-1][0];
+            snake.tail_XY[i][1] = snake.tail_XY[i-1][1];
+            gotoxy(snake.tail_XY[i][0],snake.tail_XY[i][1]);
+            cout.put('o');
+        }
+    }
+    snake.tail_XY[0][0] = snake.x;
+    snake.tail_XY[0][1] = snake.y;
 }
 void move_up()
 {
@@ -142,7 +182,7 @@ void move_right()
     gotoxy(snake.x, snake.y);
     cout.put(26);//→
     gotoxy(snake.x, snake.y);
-    check_place();
+    check_place();   
 }
 void move_left()
 {
